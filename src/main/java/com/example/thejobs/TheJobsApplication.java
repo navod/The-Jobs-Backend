2bb51e2ca5582cc2acf540b653597cec034aaf3d
navod21 <navod@commercialtp.com>
2023-07-31 19:04:31 +0530

    initialized project

---
+++ src/main/java/com/example/thejobs/TheJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.thejobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheJobsApplication.class, args);
+    }
+
+}
